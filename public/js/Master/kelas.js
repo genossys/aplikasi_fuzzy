9a6a208cc5308b954d7d0e471339d3feb7807787
bagusyanuar <bagus.yanuar613@gmail.com>
2019-05-08 21:05:42 +0700

    master kelas js
    tambah master kelas js

---
+++ public/js/Master/kelas.js
@@ -0,0 +1,22 @@
+function getDataKelas1() {
+    $('#example2').DataTable({
+        destroy     : true,
+        lengthMenu: [ [5, 10, 15, -1], [5, 10, 15, "All"] ],
+        autowidth   : true,
+        serverSide  : true,
+        processing  : false,
+        ajax        : '{{route('getDataKelas')}}',
+        columns     : [
+            {data : 'DT_RowIndex', name : 'DT_RowIndex', searchable : false, sortable : false},
+            {data :'idKelas', name   : 'idKelas'},
+            {data :'namaKelas', name : 'namaKelas'},
+            {
+                render: function (data, type, row) {
+                    return '<a class="btn-sm btn-warning" href="#" onclick="showDetail(\''+row.idKelas+'\', \''+row.namaKelas+'\');"> Edit <a/> &nbsp' +
+                        ' <a class="btn-sm btn-danger" href="#" onclick="javascript:if (confirm(\'Yakin Menghapus Data?\')) deleteDataKelas(\''+row.idKelas+'\')">Delete</a> ';
+                }
+            }
+        ]
+
+    });
+}
