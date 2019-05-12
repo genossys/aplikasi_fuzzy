6374119e6ef98764338a155df1f8fc3528fb9d9e
pradanna <pradanamahendra@gmail.com>
2019-05-12 15:21:11 +0700

    m

--- resources/views/admin/master/datakelas.blade.php
+++ resources/views/admin/master/datakelas.blade.php
@@ -75,7 +75,7 @@
                                        name="txtIdKelas">
                             </div>
                             <div class="form-group">
-                                <label>Nama Kelas</label>
+                                <label>Nama Kelasu</label>
                                 <input type="text" class="form-control" placeholder="Nama Kelas" id="txtNamaKelas"
                                        name="txtNamaKelas">
                             </div>
